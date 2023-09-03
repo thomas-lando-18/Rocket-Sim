227f45f6136d8dbd682dfa8a68a25f87dec89926
thomas-lando-18 <thomaslando6@gmail.com>
2023-09-03 13:19:53 +0930

    dev

--- stinger/app.py
+++ stinger/app.py
@@ -1,48 +1,21 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import (
-    Swagger,
-    swag_from,
-)
-import psycopg2
+from flask import Flask
+
+# Import Endpoints
+from endpoints.admin import admin_endpoints
+from endpoints.location import location_endpoints
+
 
 stinger = Flask("__name__")
-stinger.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stingeradmin:stingeradmin@localhost/stingerdb'
-db = SQLAlchemy(stinger)
-swagger = Swagger(stinger)
 
-@stinger.route("/", methods=["GET"])
-# @swag_from("swagger/main.yml")
-def heartbeat():
-    """
-    This is an endpoint that says hello :)
+# Admin/ General endpoints
+stinger.register_blueprint(admin_endpoints)
 
-    Should only be used to check that the application is running correctly.
+# Location Endpoints
+stinger.register_blueprint(location_endpoints)
 
-    """
+# Satellite Endpoints
 
-    return "Hello"
 
-@stinger.route("/about", methods=["GET"])
-def about_me():
-    """
-    Returns a description of what the appplication does.  
-    """
+# Rocket Enpdoints
 
-    # TODO Remake this using a some sort of JSON or something.
-    return "I handle the data and resource managment for the flight crew software group"
-
-@stinger.route('/check_db_connection/user/<username>/passwd/<password>/database/<db>')
-def check_db_connection(username, password, db):
-    try:
-        connection = psycopg2.connect(
-            host="localhost",
-            database=f"{db}",
-            user=f"{username}",
-            password=f"{password}"
-        )
-        return jsonify({"message": "Connected to PostgreSQL"})
-    except Exception as e:
-        return jsonify({"message": f"Error connecting to PostgreSQL: {str(e)}"})
-    
 
---
+++ stinger/endpoints/location.py
@@ -0,0 +1,11 @@
+"""
+Holds endpoints related to the location data for rocket launches and mission simulations
+"""
+
+# Imports
+from flask import Blueprint
+
+location_endpoints = Blueprint("location", __name__)
+
+
+
